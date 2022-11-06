7cb40c36374ba7b7dfcc62632299cac54173589e
hasegawa2073 <hasetatsu2001@icloud.com>
2022-11-06 21:25:27 +0900

    add: トップページのスタイルを調整した

--- nextjs-microblog/pages/index.js
+++ nextjs-microblog/pages/index.js
@@ -3,11 +3,58 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 import Layout from "../components/Layout";
+import utilStyles from "../styles/utils.module.css";
 
 export default function Home() {
   return (
     <Layout>
-      <a href="">Next.jsです</a>
+      <section className={utilStyles.headingMd}>
+        <p>フロントエンドエンジニアです。好きな言語はJavaScriptです。</p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2>📝エンジニアのブログ</h2>
+        <div className={styles.grid}>
+          <article>
+            <Link href="/">
+              <img
+                src="/images/thumbnail01.jpg"
+                className={styles.thumbnailImage}
+              />
+            </Link>
+            <Link href="/" className={utilStyles.boldText}>
+              SSGとSSRの使い分けの場合はいつなのか？
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>February 23, 2020</small>
+          </article>
+          <article>
+            <Link href="/">
+              <img
+                src="/images/thumbnail01.jpg"
+                className={styles.thumbnailImage}
+              />
+            </Link>
+            <Link href="/" className={utilStyles.boldText}>
+              SSGとSSRの使い分けの場合はいつなのか？
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>February 23, 2020</small>
+          </article>
+          <article>
+            <Link href="/">
+              <img
+                src="/images/thumbnail01.jpg"
+                className={styles.thumbnailImage}
+              />
+            </Link>
+            <Link href="/" className={utilStyles.boldText}>
+              SSGとSSRの使い分けの場合はいつなのか？
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>February 23, 2020</small>
+          </article>
+        </div>
+      </section>
     </Layout>
   );
 }
