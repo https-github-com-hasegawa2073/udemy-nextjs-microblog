0add4221d4a282a29e08f5a83d44f6ef6c6ef197
hasegawa2073 <hasetatsu2001@icloud.com>
2022-11-07 15:40:17 +0900

    fix: 記事のページでホームへ戻るボタン追加

--- nextjs-microblog/pages/index.js
+++ nextjs-microblog/pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
-import Layout from "../components/Layout";
+import Layout, { siteTitle } from "../components/Layout";
 import utilStyles from "../styles/utils.module.css";
 import { getPostsData } from "../lib/post";
 
@@ -20,7 +20,10 @@ export async function getStaticProps() {
 export default function Home({ allPostData }) {
   console.log(allPostData);
   return (
-    <Layout>
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
       <section className={utilStyles.headingMd}>
         <p>フロントエンドエンジニアです。好きな言語はJavaScriptです。</p>
       </section>
